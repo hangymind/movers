ba8db271b81c63ccb9b96d756ef510a9b04066ec
XiaoYiw46 <154738703+hangymind@users.noreply.github.com>
2026-03-13 23:58:30 +0800

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,3 @@
+ setInterval(function() {
+    console.log("%cMOV调试功能已加载！这是用来更新时间戳来刷新缓存的文件。您通常会看到这条消息，不过请您忽略它。如果您在控制台没有看见它，请马上联系开发者！", "background: #0095B6; color: #fff; border-radius: 2px; padding: 2px;");
+    }, 5000);
